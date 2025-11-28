b8e18bc7205a7fa79c82e9c8a128861bd6eb21ba
Assanali <assanalimarwa@gmail.com>
2025-11-28 21:36:52 +0500

    new

--- convnextblock.py
+++ convnextblock.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn 
+import torch.nn.functional as F
 from timm.models.layers import trunc_normal_, DropPath
 from timm.models.registry import register_model
 from layernorm import LayerNorm
