b40164407b6ac383dc6c6382e36ad92f59c6e120
Assanali <assanalimarwa@gmail.com>
2025-12-03 14:28:50 +0500

    new

--- datasets/trainer.py
+++ datasets/trainer.py
@@ -22,6 +22,7 @@ def trainer_acdc(model):
     num_classes = 4
     batch_size = 4
     img_size = 224
+    max_epoch = 100
     max_iterations = max_epoch * 1500 // batch_size
 
     if torch.cuda.is_available():
