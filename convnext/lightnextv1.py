f428fbccf4b1b4fb89f3b18358b976d4b579cde5
Assanali <assanalimarwa@gmail.com>
2025-12-05 20:01:50 +0500

    new

--- convnext/lightnextv1.py
+++ convnext/lightnextv1.py
@@ -213,7 +213,7 @@ class LayerNorm(nn.Module):
             return x
 
 
-class LightNext(nn.Module):
+class LightNextv1(nn.Module):
     def __init__(self, in_chans=1, num_classes=4, 
                  depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], 
                  drop_path_rate=0., layer_scale_init_value=1e-6):
