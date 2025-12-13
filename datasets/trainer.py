1524014b3ee0aeaf2b9ea02c517253823a4f9f66
assanalimarwa <assanalimarwa@gmail.com>
2025-12-13 15:06:43 +0500

    new

--- datasets/trainer.py
+++ datasets/trainer.py
@@ -16,8 +16,32 @@ from utils.utilities import DiceLoss
 from torchvision import transforms
 from utils.utilities import DiceLoss, test_single_volume, calculate_metric_percase
 
-def trainer_acdc(model, model_type: str):
+
+
+def set_seed(seed=42):
+    """Set all random seeds for reproducibility"""
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    
+    # Make CUDA deterministic
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    print(f"✅ All seeds set to {seed}")
+
+
+def trainer_acdc(model, model_type: str, seed=42):
     from datasets.dataset_acdc import BaseDataSets, RandomGenerator
+    
+    # ============================================
+    # SET SEED FIRST (BEFORE ANY RANDOM OPERATIONS)
+    # ============================================
+    set_seed(seed)
+    
     base_lr = 0.001
     num_classes = 4
     batch_size = 4
@@ -26,48 +50,82 @@ def trainer_acdc(model, model_type: str):
     max_iterations = max_epoch * 1500 // batch_size
 
     if torch.cuda.is_available():
-    # If available, set the device to 'cuda' (GPU)
         device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
 
     snapshot_path = '/home/user/lightnext/snapshot/'
 
-    db_train = BaseDataSets(base_dir='/home/user/lightnext/datasets/ACDC', split="train", transform=transforms.Compose([
-        RandomGenerator([224, 224])]))
-    db_val = BaseDataSets(base_dir='/home/user/lightnext/datasets/ACDC', split="val")
+    db_train = BaseDataSets(
+        base_dir='/home/user/lightnext/datasets/ACDC', 
+        split="train", 
+        transform=transforms.Compose([RandomGenerator([224, 224])])
+    )
+    db_val = BaseDataSets(
+        base_dir='/home/user/lightnext/datasets/ACDC', 
+        split="val"
+    )
+    
     def worker_init_fn(worker_id):
-        random.seed(1234 + worker_id)
-    trainloader = DataLoader(db_train, batch_size=batch_size, shuffle=True,
-                             num_workers=8, pin_memory=True, worker_init_fn=worker_init_fn)
-    valloader = DataLoader(db_val, batch_size=1, shuffle=False,
-                           num_workers=1)
+        # Seed each worker with base seed + worker_id
+        worker_seed = seed + worker_id
+        random.seed(worker_seed)
+        np.random.seed(worker_seed)
+        torch.manual_seed(worker_seed)
+    
+    trainloader = DataLoader(
+        db_train, 
+        batch_size=batch_size, 
+        shuffle=True,
+        num_workers=8, 
+        pin_memory=True, 
+        worker_init_fn=worker_init_fn
+    )
+    
+    valloader = DataLoader(
+        db_val, 
+        batch_size=1, 
+        shuffle=False,
+        num_workers=1
+    )
+    
     model.to(device)
     model.train()
-    optimizer = optim.SGD(model.parameters(), lr=base_lr,
-                          momentum=0.9, weight_decay=0.0001)
+    
+    optimizer = optim.SGD(
+        model.parameters(), 
+        lr=base_lr,
+        momentum=0.9, 
+        weight_decay=0.0001
+    )
+    
     ce_loss = CrossEntropyLoss(ignore_index=4)
     dice_loss = DiceLoss(num_classes)
 
     writer = SummaryWriter(snapshot_path + '/log')
     logging.info("{} iterations per epoch".format(len(trainloader)))
     logging.info("{} val iterations per epoch".format(len(valloader)))
-    # logging.info("{} test iterations per epoch".format(len(testloader)))
 
     iter_num = 0
     max_epoch = 100
     best_performance = 0.0
     iterator = tqdm(range(max_epoch), ncols=70)
+    
     for epoch_num in iterator:
         for i_batch, sampled_batch in enumerate(trainloader):
             volume_batch, label_batch = sampled_batch['image'], sampled_batch['label']
             volume_batch, label_batch = volume_batch.to(device), label_batch.to(device)
+            
             outputs = model(volume_batch)
             loss_ce = ce_loss(outputs, label_batch[:].long())
             loss_dice = dice_loss(outputs, label_batch, softmax=True)
             loss = 0.5 * loss_ce + 0.5 * loss_dice
+            
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            # to change in near future
+            
+            # Learning rate decay
             lr_ = float(base_lr * (1.0 - float(iter_num) / float(max_iterations)) ** 0.9)
             for param_group in optimizer.param_groups:
                 param_group['lr'] = lr_
@@ -79,47 +137,200 @@ def trainer_acdc(model, model_type: str):
 
             logging.info('iteration %d : loss : %f, loss_ce: %f' % (iter_num, loss.item(), loss_ce.item()))
 
+            # Visualization
             if iter_num % 20 == 0:
                 image = volume_batch[1, 0:1, :, :]
                 image = (image - image.min()) / (image.max() - image.min())
                 writer.add_image('train/Image', image, iter_num)
-                outputs = torch.argmax(torch.softmax(
-                    outputs, dim=1), dim=1, keepdim=True)
-                writer.add_image('train/Prediction',
-                                 outputs[1, ...] * 50, iter_num)
+                outputs = torch.argmax(torch.softmax(outputs, dim=1), dim=1, keepdim=True)
+                writer.add_image('train/Prediction', outputs[1, ...] * 50, iter_num)
                 labs = label_batch[1, ...].unsqueeze(0) * 50
                 writer.add_image('train/GroundTruth', labs, iter_num)
 
-            if iter_num > 0 and iter_num % 500 == 0:  # 500
+            # Validation
+            if iter_num > 0 and iter_num % 500 == 0:
                 model.eval()
                 metric_list = 0.0
+                
                 for i_batch, sampled_batch in enumerate(valloader):
                     image, label = sampled_batch["image"], sampled_batch["label"]
-                    metric_i = test_single_volume(image, label, model, classes=num_classes,
-                                                  patch_size=[img_size, img_size])
+                    metric_i = test_single_volume(
+                        image, label, model, 
+                        classes=num_classes,
+                        patch_size=[img_size, img_size]
+                    )
                     metric_list += np.array(metric_i)
+                
                 metric_list = metric_list / len(db_val)
+                
                 for class_i in range(num_classes - 1):
-                    writer.add_scalar('info/val_{}_dice'.format(class_i + 1),
-                                      metric_list[class_i, 0], iter_num)
-                    writer.add_scalar('info/val_{}_hd95'.format(class_i + 1),
-                                      metric_list[class_i, 1], iter_num)
+                    writer.add_scalar(
+                        'info/val_{}_dice'.format(class_i + 1),
+                        metric_list[class_i, 0], 
+                        iter_num
+                    )
+                    writer.add_scalar(
+                        'info/val_{}_hd95'.format(class_i + 1),
+                        metric_list[class_i, 1], 
+                        iter_num
+                    )
 
                 performance = np.mean(metric_list, axis=0)[0]
-
                 mean_hd95 = np.mean(metric_list, axis=0)[1]
+                
                 writer.add_scalar('info/val_mean_dice', performance, iter_num)
                 writer.add_scalar('info/val_mean_hd95', mean_hd95, iter_num)
 
                 if performance > best_performance:
-                    best_iteration, best_performance, best_hd95 = iter_num, performance, mean_hd95
-                    save_best = os.path.join(snapshot_path, 'orig_model.pth' if model_type == 'orig' else 'new_model.pth')
-                    torch.save(model.state_dict(), save_best)
-                    logging.info('Best model | iteration %d : mean_dice : %f mean_hd95 : %f' % (
-                    iter_num, performance, mean_hd95))
+                    best_iteration = iter_num
+                    best_performance = performance
+                    best_hd95 = mean_hd95
+                    
+                    save_best = os.path.join(
+                        snapshot_path, 
+                        'orig_model1.pth' if model_type == 'orig' else 'new_model1.pth'
+                    )
+                    
+                    # Save with seed info
+                    torch.save({
+                        'model_state_dict': model.state_dict(),
+                        'config': {
+                            'in_channels': 1,
+                            'n_channels': 32,
+                            'n_classes': 4,
+                            'exp_r': [2,3,4,4,4,4,4,3,2],
+                            'kernel_size': 3,
+                            'deep_supervision': False,
+                            'do_res': True,
+                            'do_res_up_down': True,
+                            'block_counts': [2,2,2,2,2,2,2,2,2]
+                        },
+                        'seed': seed,
+                        'best_dice': performance,
+                        'best_hd95': mean_hd95,
+                        'iteration': iter_num
+                    }, save_best)
+                    
+                    logging.info(
+                        'Best model | iteration %d : mean_dice : %f mean_hd95 : %f' % 
+                        (iter_num, performance, mean_hd95)
+                    )
 
-                logging.info('iteration %d : mean_dice : %f mean_hd95 : %f' % (iter_num, performance, mean_hd95))
+                logging.info(
+                    'iteration %d : mean_dice : %f mean_hd95 : %f' % 
+                    (iter_num, performance, mean_hd95)
+                )
                 model.train()
+    
+    writer.close()
+    return best_performance, best_hd95
+
+
+
+# def trainer_acdc(model, model_type: str):
+#     from datasets.dataset_acdc import BaseDataSets, RandomGenerator
+#     base_lr = 0.001
+#     num_classes = 4
+#     batch_size = 4
+#     img_size = 224
+#     max_epoch = 100
+#     max_iterations = max_epoch * 1500 // batch_size
+
+#     if torch.cuda.is_available():
+#     # If available, set the device to 'cuda' (GPU)
+#         device = torch.device("cuda")
+
+#     snapshot_path = '/home/user/lightnext/snapshot/'
+
+#     db_train = BaseDataSets(base_dir='/home/user/lightnext/datasets/ACDC', split="train", transform=transforms.Compose([
+#         RandomGenerator([224, 224])]))
+#     db_val = BaseDataSets(base_dir='/home/user/lightnext/datasets/ACDC', split="val")
+#     def worker_init_fn(worker_id):
+#         random.seed(1234 + worker_id)
+#     trainloader = DataLoader(db_train, batch_size=batch_size, shuffle=True,
+#                              num_workers=8, pin_memory=True, worker_init_fn=worker_init_fn)
+#     valloader = DataLoader(db_val, batch_size=1, shuffle=False,
+#                            num_workers=1)
+#     model.to(device)
+#     model.train()
+#     optimizer = optim.SGD(model.parameters(), lr=base_lr,
+#                           momentum=0.9, weight_decay=0.0001)
+#     ce_loss = CrossEntropyLoss(ignore_index=4)
+#     dice_loss = DiceLoss(num_classes)
+
+#     writer = SummaryWriter(snapshot_path + '/log')
+#     logging.info("{} iterations per epoch".format(len(trainloader)))
+#     logging.info("{} val iterations per epoch".format(len(valloader)))
+#     # logging.info("{} test iterations per epoch".format(len(testloader)))
+
+#     iter_num = 0
+#     max_epoch = 100
+#     best_performance = 0.0
+#     iterator = tqdm(range(max_epoch), ncols=70)
+#     for epoch_num in iterator:
+#         for i_batch, sampled_batch in enumerate(trainloader):
+#             volume_batch, label_batch = sampled_batch['image'], sampled_batch['label']
+#             volume_batch, label_batch = volume_batch.to(device), label_batch.to(device)
+#             outputs = model(volume_batch)
+#             loss_ce = ce_loss(outputs, label_batch[:].long())
+#             loss_dice = dice_loss(outputs, label_batch, softmax=True)
+#             loss = 0.5 * loss_ce + 0.5 * loss_dice
+#             optimizer.zero_grad()
+#             loss.backward()
+#             optimizer.step()
+#             # to change in near future
+#             lr_ = float(base_lr * (1.0 - float(iter_num) / float(max_iterations)) ** 0.9)
+#             for param_group in optimizer.param_groups:
+#                 param_group['lr'] = lr_
+
+#             iter_num = iter_num + 1
+#             writer.add_scalar('info/lr', lr_, iter_num)
+#             writer.add_scalar('info/total_loss', loss, iter_num)
+#             writer.add_scalar('info/loss_ce', loss_ce, iter_num)
+
+#             logging.info('iteration %d : loss : %f, loss_ce: %f' % (iter_num, loss.item(), loss_ce.item()))
+
+#             if iter_num % 20 == 0:
+#                 image = volume_batch[1, 0:1, :, :]
+#                 image = (image - image.min()) / (image.max() - image.min())
+#                 writer.add_image('train/Image', image, iter_num)
+#                 outputs = torch.argmax(torch.softmax(
+#                     outputs, dim=1), dim=1, keepdim=True)
+#                 writer.add_image('train/Prediction',
+#                                  outputs[1, ...] * 50, iter_num)
+#                 labs = label_batch[1, ...].unsqueeze(0) * 50
+#                 writer.add_image('train/GroundTruth', labs, iter_num)
+
+#             if iter_num > 0 and iter_num % 500 == 0:  # 500
+#                 model.eval()
+#                 metric_list = 0.0
+#                 for i_batch, sampled_batch in enumerate(valloader):
+#                     image, label = sampled_batch["image"], sampled_batch["label"]
+#                     metric_i = test_single_volume(image, label, model, classes=num_classes,
+#                                                   patch_size=[img_size, img_size])
+#                     metric_list += np.array(metric_i)
+#                 metric_list = metric_list / len(db_val)
+#                 for class_i in range(num_classes - 1):
+#                     writer.add_scalar('info/val_{}_dice'.format(class_i + 1),
+#                                       metric_list[class_i, 0], iter_num)
+#                     writer.add_scalar('info/val_{}_hd95'.format(class_i + 1),
+#                                       metric_list[class_i, 1], iter_num)
+
+#                 performance = np.mean(metric_list, axis=0)[0]
+
+#                 mean_hd95 = np.mean(metric_list, axis=0)[1]
+#                 writer.add_scalar('info/val_mean_dice', performance, iter_num)
+#                 writer.add_scalar('info/val_mean_hd95', mean_hd95, iter_num)
+
+#                 if performance > best_performance:
+#                     best_iteration, best_performance, best_hd95 = iter_num, performance, mean_hd95
+#                     save_best = os.path.join(snapshot_path, 'orig_model.pth' if model_type == 'orig' else 'new_model.pth')
+#                     torch.save(model.state_dict(), save_best)
+#                     logging.info('Best model | iteration %d : mean_dice : %f mean_hd95 : %f' % (
+#                     iter_num, performance, mean_hd95))
+
+#                 logging.info('iteration %d : mean_dice : %f mean_hd95 : %f' % (iter_num, performance, mean_hd95))
+#                 model.train()
 
             
 
--- tests.py
+++ tests.py
@@ -5,37 +5,165 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 from convnext.lightnext import LightNext
 from utils.utilities import test_single_volume
-from convnext.lightnextv1 import LightNextv1
+from convnext.mednext.mednextorig import MedNeXt
+from convnext.mednext.mednextnew import MedNeXtNew
 
 
 
+
+# def inference_acdc(model, model_path='best_model.pth'):
+#     from datasets.dataset_acdc import BaseDataSets
+    
+#     # Setup logging
+#     logging.basicConfig(level=logging.INFO,
+#                        format='%(asctime)s - %(levelname)s - %(message)s')
+    
+#     # Parameters
+#     num_classes = 4
+#     img_size = 224
+#     base_dir = '/home/user/lightnext/datasets/ACDC'
+#     test_save_path = '/home/user/lightnext/predictions/'
+    
+#     # Create test save directory
+#     import os
+#     os.makedirs(test_save_path, exist_ok=True)
+    
+#     # Device setup
+#     if torch.cuda.is_available():
+#         device = torch.device("cuda")
+#     else:
+#         device = torch.device("cpu")
+    
+#     # Load model
+#     logging.info(f"Loading model from {model_path}")
+#     model.load_state_dict(torch.load(model_path, map_location=device))
+#     model.to(device)
+#     model.eval()
+    
+#     # Load test dataset
+#     db_test = BaseDataSets(base_dir=base_dir, split="test_vol")
+#     testloader = DataLoader(db_test, batch_size=1, shuffle=False, num_workers=1)
+    
+#     logging.info("{} test iterations per epoch".format(len(testloader)))
+    
+#     metric_list = 0.0
+    
+#     with torch.no_grad():
+#         for i_batch, sampled_batch in tqdm(enumerate(testloader), total=len(testloader)):
+#             image, label, case_name = sampled_batch["image"], sampled_batch["label"], sampled_batch['case_name'][0]
+            
+#             metric_i = test_single_volume(
+#                 image, label, model, 
+#                 classes=num_classes, 
+#                 patch_size=[img_size, img_size],
+#                 test_save_path=test_save_path, 
+#                 case=case_name, 
+#                 z_spacing=1.0  # Adjust if needed
+#             )
+            
+#             metric_list += np.array(metric_i)
+            
+#             logging.info('idx %d case %s mean_dice %f mean_hd95 %f' % 
+#                         (i_batch, case_name, np.mean(metric_i, axis=0)[0], np.mean(metric_i, axis=0)[1]))
+    
+#     # Calculate average metrics
+#     metric_list = metric_list / len(db_test)
+    
+#     # Log per-class metrics
+#     for i in range(1, num_classes):
+#         logging.info('Mean class %d mean_dice %f mean_hd95 %f' % 
+#                     (i, metric_list[i-1][0], metric_list[i-1][1]))
+    
+#     # Overall performance
+#     performance = np.mean(metric_list, axis=0)[0]
+#     mean_hd95 = np.mean(metric_list, axis=0)[1]
+    
+#     logging.info('Testing performance in best val model: mean_dice : %f mean_hd95 : %f' % 
+#                 (performance, mean_hd95))
+    
+#     return performance, mean_hd95
+
+
+
+
+
+# def create_mednextv1_base_orig(num_input_channels, num_classes, kernel_size=3, ds=False):
+
+#     return MedNeXt(
+#         in_channels = num_input_channels, 
+#         n_channels = 32,
+#         n_classes = num_classes, 
+#         exp_r=[2,3,4,4,4,4,4,3,2],       
+#         kernel_size=kernel_size,         
+#         deep_supervision=ds,             
+#         do_res=True,                     
+#         do_res_up_down = True,
+#         block_counts = [2,2,2,2,2,2,2,2,2]
+
+        
+#     )
+
+
+# model_orig = create_mednextv1_base_orig(1, 4)
+
+
+
+# # Run inference
+# model_path = '/home/user/lightnext/snapshot/orig_model1.pth'
+# dice_score, hd95_score = inference_acdc(model_orig, model_path)
+
+# print(f"Final Results - Dice: {dice_score:.4f}, HD95: {hd95_score:.4f}")
+# print(123)
+
+
+
+import torch
+import numpy as np
+import logging
+from convnext.mednext.mednextorig import MedNeXt
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 def inference_acdc(model, model_path='best_model.pth'):
     from datasets.dataset_acdc import BaseDataSets
+    from utils.utilities import test_single_volume
+    import os
     
-    # Setup logging
-    logging.basicConfig(level=logging.INFO,
-                       format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
     
-    # Parameters
     num_classes = 4
     img_size = 224
     base_dir = '/home/user/lightnext/datasets/ACDC'
     test_save_path = '/home/user/lightnext/predictions/'
-    
-    # Create test save directory
-    import os
     os.makedirs(test_save_path, exist_ok=True)
     
-    # Device setup
-    if torch.cuda.is_available():
-        device = torch.device("cuda")
-    else:
-        device = torch.device("cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
-    # Load model
+    # Load checkpoint
     logging.info(f"Loading model from {model_path}")
-    model.load_state_dict(torch.load(model_path, map_location=device))
+    checkpoint = torch.load(
+        model_path, 
+        map_location=device, 
+        weights_only=False  # ← FIX: Allow non-tensor objects
+    )
+    
+    # Handle both formats
+    if isinstance(checkpoint, dict):
+        if 'model_state_dict' in checkpoint:
+            # Your new format (with config)
+            state_dict = checkpoint['model_state_dict']
+            logging.info(f"Loaded checkpoint from iteration {checkpoint.get('iteration', 'unknown')}")
+            logging.info(f"Validation Dice: {checkpoint.get('best_dice', 'unknown'):.4f}")
+        else:
+            # Old format (just state_dict)
+            state_dict = checkpoint
+    else:
+        state_dict = checkpoint
+    
+    model.load_state_dict(state_dict)
     model.to(device)
     model.eval()
     
@@ -43,13 +171,15 @@ def inference_acdc(model, model_path='best_model.pth'):
     db_test = BaseDataSets(base_dir=base_dir, split="test_vol")
     testloader = DataLoader(db_test, batch_size=1, shuffle=False, num_workers=1)
     
-    logging.info("{} test iterations per epoch".format(len(testloader)))
+    logging.info(f"{len(testloader)} test cases")
     
     metric_list = 0.0
     
     with torch.no_grad():
         for i_batch, sampled_batch in tqdm(enumerate(testloader), total=len(testloader)):
-            image, label, case_name = sampled_batch["image"], sampled_batch["label"], sampled_batch['case_name'][0]
+            image = sampled_batch["image"]
+            label = sampled_batch["label"]
+            case_name = sampled_batch['case_name'][0]
             
             metric_i = test_single_volume(
                 image, label, model, 
@@ -57,47 +187,70 @@ def inference_acdc(model, model_path='best_model.pth'):
                 patch_size=[img_size, img_size],
                 test_save_path=test_save_path, 
                 case=case_name, 
-                z_spacing=1.0  # Adjust if needed
+                z_spacing=1.0
             )
             
             metric_list += np.array(metric_i)
             
-            logging.info('idx %d case %s mean_dice %f mean_hd95 %f' % 
-                        (i_batch, case_name, np.mean(metric_i, axis=0)[0], np.mean(metric_i, axis=0)[1]))
+            logging.info(
+                f'Case {i_batch} ({case_name}): '
+                f'Dice={np.mean(metric_i, axis=0)[0]:.4f}, '
+                f'HD95={np.mean(metric_i, axis=0)[1]:.4f}'
+            )
     
-    # Calculate average metrics
+    # Average metrics
     metric_list = metric_list / len(db_test)
     
-    # Log per-class metrics
+    # Per-class results
     for i in range(1, num_classes):
-        logging.info('Mean class %d mean_dice %f mean_hd95 %f' % 
-                    (i, metric_list[i-1][0], metric_list[i-1][1]))
+        logging.info(
+            f'Class {i}: Dice={metric_list[i-1][0]:.4f}, '
+            f'HD95={metric_list[i-1][1]:.4f}'
+        )
     
     # Overall performance
     performance = np.mean(metric_list, axis=0)[0]
     mean_hd95 = np.mean(metric_list, axis=0)[1]
     
-    logging.info('Testing performance in best val model: mean_dice : %f mean_hd95 : %f' % 
-                (performance, mean_hd95))
+    logging.info('='*60)
+    logging.info(f'FINAL TEST RESULTS:')
+    logging.info(f'  Mean Dice: {performance:.4f}')
+    logging.info(f'  Mean HD95: {mean_hd95:.4f}')
+    logging.info('='*60)
     
     return performance, mean_hd95
 
 
-# Usage:
+# ============================================
+# USAGE
+# ============================================
 
-    
-    # Initialize model with same parameters as training
-model = LightNextv1(
-    in_chans=1, 
-    num_classes=4, 
-    depths=[3, 3, 9, 3], 
-    dims=[96, 192, 384, 768]
-)
+def create_mednextv1_base_new(num_input_channels, num_classes, kernel_size=3, ds=False):
+
+    return MedNeXtNew(
+        in_channels = num_input_channels, 
+        n_channels = 32,
+        n_classes = num_classes, 
+        exp_r=[2,3,4,4,4,4,4,3,2],       
+        kernel_size=kernel_size,         
+        deep_supervision=ds,             
+        do_res=True,                     
+        do_res_up_down = True,
+        block_counts = [2,2,2,2,2,2,2,2,2]
+
+        
+    )
+
+
+model_new = create_mednextv1_base_new(1, 4)
+
 
 # Run inference
-model_path = '/home/user/lightnext/snapshot/best_model.pth'
-dice_score, hd95_score = inference_acdc(model, model_path)
+model_path = '/home/user/lightnext/snapshot/new_model1.pth'
+dice_score, hd95_score = inference_acdc(model_new, model_path)
 
-print(f"Final Results - Dice: {dice_score:.4f}, HD95: {hd95_score:.4f}")
-print(123)
-
+print("\n" + "="*60)
+print(f"Final Test Results:")
+print(f"  Dice Score: {dice_score:.4f}")
+print(f"  HD95 Score: {hd95_score:.4f}")
+print("="*60)
